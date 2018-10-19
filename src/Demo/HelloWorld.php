82cc1b137339608bca0e06579e7d4c41998526f9
3A532017 <s3a532017@student.ncut.edu.tw>
2018-10-19 11:39:54 +0800

    第三周作業HelloWorld

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0106
+ * Date: 2018/10/10
+ * Time: 上午 05:43
+ */
+
+      namespace Demo;
+
+      class HelloWorld
+      {
+          function __construct()
+          {
+              echo 'Hello World ~'.PHP_EOL;
+          }
+      }
