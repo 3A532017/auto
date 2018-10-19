cbf06770b2c4aaeac4adc58997598a415d482377
3A532017 <s3a532017@student.ncut.edu.tw>
2018-10-19 11:40:12 +0800

    第三周作業Someone

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0106
+ * Date: 2018/10/10
+ * Time: 上午 05:44
+ */
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
