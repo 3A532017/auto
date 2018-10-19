639550ab7e82fcbd89d8ff737cd1ed749cbc4143
3A532017 <s3a532017@student.ncut.edu.tw>
2018-10-19 11:40:03 +0800

    第三周作業Lara

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 0106
+ * Date: 2018/10/10
+ * Time: 上午 05:43
+ */
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
